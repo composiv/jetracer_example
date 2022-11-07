f9bf31dcd1989f7dadf59b1e578bfc2672546e70
Naci Dai <nacidai@acm.org>
2022-11-07 13:57:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export ROS_PYTHON_VERSION=3
-ROS_PYTHON_VERSION=3 source devel/setup.bash && roslaunch launch/muto.launch
+ROS_PYTHON_VERSION=3 source /opt/ros/melodic/setup.bash && source devel/setup.bash && roslaunch launch/muto.launch
