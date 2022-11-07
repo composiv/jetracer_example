2af1d726d3a5918dd3cf267ea3e17f9c72ab3187
Naci Dai <nacidai@acm.org>
2022-11-07 18:38:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@ curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo ap
 sudo apt-get update
 sudo apt-get -y install ros-melodic-desktop
 sudo apt-get -y install ros-melodic-tf2-geometry-msgs ros-melodic-ackermann-msgs ros-melodic-joy ros-melodic-map-server ros-melodic-usb-cam ros-melodic-web-video-server ros-melodic-rosbridge-server 
-sudo apt-get -y install python-catkin-tools python3-dev python3-numpy
+sudo apt-get -y install python-catkin-tools python3-dev python3-numpy python-pip python3-pip
 sudo apt-get -y install  i2c-tools libi2c-dev
 
 pip3 install rospkg catkin_pkg empy  requests paho-mqtt
 pip2 install  requests paho-mqtt
+
 sudo ln -s /usr/include/opencv4 /usr/include/opencv || true
 
 git clone --branch main https://github.com/eclipse-muto/messages.git  src/messages
