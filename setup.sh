daa6dd5bcde6ccab6f4176daa533546fb346829b
Naci Dai <nacidai@acm.org>
2022-11-07 17:26:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-get install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt-get update
 sudo apt-get -y install ros-melodic-desktop
